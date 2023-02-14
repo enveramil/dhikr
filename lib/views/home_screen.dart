29f6bd98a730c57248e3b94d24185fcaa48a8636
Yusuf Güzel <guzelyusuf10@gmail.com>
2023-02-14 13:43:53 +0300

    deneme

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -133,7 +133,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         const SizedBox(
-          height: 20,
+          height: 21,
         ),
         Center(
           child: Row(
